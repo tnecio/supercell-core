316c8f5555b1a1128b6ae11e939df94375231a21
Tomasz Necio <tomasz.necio@gmail.com>
2020-05-01 23:54:44 +0200

    Updated version to 0.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setuptools.setup(
     name='supercell_core',
-    version='0.0.6',
+    version='0.1.0',
     packages=setuptools.find_packages(),
     url='https://github.com/tnecio/supercell-core',
     license='GPLv3',
