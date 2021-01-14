tnecio/supercell-core

commit 9b5fe2ca80241c838e38f3cf01f461485bbfbe99

Performance improvement, and name change (moire/brute to fast/direct)

supercell_core/heterostructure_opt.py
@@ -39,7 +39,7 @@ class OptSolverConfig:
 class OptSolver:
     """
 
-    TODO
+    TODO documentation
     """
 
     def __init__(self,
@@ -58,6 +58,7 @@ class OptSolver:
                 "max_strain",
                 "supercell_size",
                 "M_11", "M_12", "M_21", "M_22",
+                *["N{}_{}{}".format(n+1, i+1, j+1) for n in range(len(XBs)) for i in range(2) for j in range(2)],
                 "supercell_vectors_11", "supercell_vectors_12",
                 "supercell_vectors_21", "supercell_vectors_22",
                 *["strain_tensor_layer_{}_{}{}".format(k + 1, i + 1, j + 1)
@@ -67,7 +68,7 @@ class OptSolver:
             for column in columns:
                 self.log[column] = []
             if pd is None:
-                print("Pandas not installed! Returning a dictionary instead (log)")
+                print("Pandas not installed! Returning a dictionary instead (log)") # Jezu jak źle
 
         # Prepare all possible dt vectors (in A basis)
         self.dt_As = np.array([])
@@ -119,7 +120,8 @@ class OptSolver:
     def _update_opt_res(self,
                         thetas: Tuple[Angle, ...],
                         ADt: np.ndarray,
-                        sts: List[Matrix2x2]
+                        sts: List[Matrix2x2],
+                        XBrs: List[Matrix2x2]
                         ) -> None:
         """
         Checks if newly calculated result is better than the previous one
@@ -143,6 +145,14 @@ class OptSolver:
         new_res = (list(thetas), min_qty, XDt, sts)
         new_size = np.abs(np.linalg.det(XDt))
         if self.config.log:
+
+            for n, XBr in enumerate(XBrs):
+                BrDt = inv(XBr) @ self.XA @ ADt
+                BtrDt = np.round(BrDt)
+                for i in range(2):
+                    for j in range(2):
+                        self.log["N{}_{}{}".format(n+1, i+1, j+1)].append(BtrDt[i, j])
+
             for i, theta in enumerate(thetas):
                 self.log["theta_{}".format(i)].append(theta)
             self.log["max_strain"].append(min_qty)
@@ -319,7 +329,7 @@ class MoireFinder(OptSolver):
         memo = {}
         for i, (theta_, XB) in enumerate(zip(self.thetas, self.XBs)):
             for theta in theta_:
-                qtys = self._moire(XB, theta)
+                qtys = self._fast(XB, theta)
                 memo[float(theta), i] = qtys
 
         # We need to have this loop over all possible combinations since
@@ -359,11 +369,11 @@ class MoireFinder(OptSolver):
             sts = [self._calculate_strain_tensor(ADt, XBr) for XBr in XBrs]
             # TODO: move to update_opt_res
 
-            self._update_opt_res(theta_comb, ADt, sts)
+            self._update_opt_res(theta_comb, ADt, sts, XBrs)
 
         return self.get_result()
 
-    def _moire(self, XB: Matrix2x2, theta: float) -> np.ndarray:
+    def _fast(self, XB: Matrix2x2, theta: float) -> np.ndarray:
         """
         Calculates qualities of each dt vector in dt_As,
         where the measure of quality is a heuristic of
@@ -415,7 +425,7 @@ class MoireFinder(OptSolver):
             return False
 
         # 2. Is det in any other basis == 0?
-        # This can happen when the solution to the equation in `_moire` is bad and gives
+        # This can happen when the solution to the equation in `_fast` is bad and gives
         # e.g. values close to zero; this translates to impossible stretching of Br
         XBrs = [rotate(XB, theta) for XB, theta in zip(self.XBs, thetas)]
         BrDts = [inv(XBr) @ self.XA @ ADt for XBr in XBrs]

supercell_core/heterostructure.py
@@ -257,9 +257,28 @@ class Heterostructure:
 
     ### CALC METHODS
 
+    def __calc_strain_tensor_from_ADt(self, ADt: Matrix2x2, XBr: Matrix2x2) -> Matrix2x2:
+        """
+        Calculate strain tensor.
+
+        See docs of `Heterostructure.calc` for definition of strain tensor.
+
+        Parameters
+        ----------
+        ADt : Matrix 2x2
+        XBr : Matrix 2x2
+
+        Returns
+        -------
+        Matrix 2x2
+        """
+        BrDt, BtrBr, XXt = self.__get_basis_change_matrices(ADt, XBr)
+        return Heterostructure.__calc_strain_tensor(XBr, XXt)
+
     def calc(self,
              M=((1, 0), (0, 1)),
-             thetas=None) -> Result:
+             thetas=None,
+             calc_atoms=True) -> Result:
         """
         Calculates strain tensor and other properties of the system in given
         circumstances
@@ -320,25 +339,7 @@ class Heterostructure:
             thetas = [lay_desc[1][0] for lay_desc in self.__layers]
 
         ADt = np.array(M)
-        return self.__calc_aux(ADt, thetas)
-
-    def __calc_strain_tensor_from_ADt(self, ADt: Matrix2x2, XBr: Matrix2x2) -> Matrix2x2:
-        """
-        Calculate strain tensor.
-
-        See docs of `Heterostructure.calc` for definition of strain tensor.
-
-        Parameters
-        ----------
-        ADt : Matrix 2x2
-        XBr : Matrix 2x2
-
-        Returns
-        -------
-        Matrix 2x2
-        """
-        BrDt, BtrBr, XXt = self.__get_basis_change_matrices(ADt, XBr)
-        return Heterostructure.__calc_strain_tensor(XBr, XXt)
+        return self.__calc_aux(ADt, thetas, calc_atoms=calc_atoms)
 
     def __get_basis_change_matrices(self, ADt, XBr):
         """
@@ -363,7 +364,8 @@ class Heterostructure:
 
     def __calc_aux(self,
                    ADt: InMatrix2x2,
-                   thetas: List[Angle]) -> Result:
+                   thetas: List[Angle],
+                   calc_atoms: bool) -> Result:
         """
         Calculates strain tensor and other properties of the system in given
         circumstances
@@ -401,7 +403,14 @@ class Heterostructure:
         strain_tensors_wiki = [Heterostructure.__get_strain_tensor_wiki(XXt)
                                for XXt in XXts]
 
-        superlattice = self.__build_superlattice(XA @ ADt, ADt, BrDts)
+
+        superlattice = self.__build_superlattice(XA @ ADt, ADt, BrDts, calc_atoms)
+
+        A_atoms_per_el_cell = len(self.__substrate.atoms())
+        atom_count = np.abs(np.linalg.det(ADt) * A_atoms_per_el_cell)
+        for layer, BrDt in zip(self.__layers, BrDts):
+            B_atoms_per_el_cell = len(layer[0].atoms())
+            atom_count += np.round(np.abs(np.linalg.det(BrDt) * B_atoms_per_el_cell))
 
         return Result(
             self,
@@ -410,7 +419,8 @@ class Heterostructure:
             strain_tensors,
             strain_tensors_wiki,
             ADt,
-            ABtrs
+            ABtrs,
+            atom_count = atom_count
         )
 
     @staticmethod
@@ -442,7 +452,7 @@ class Heterostructure:
         return F
 
     def __build_superlattice(self, XDt: Matrix2x2, ADt: Matrix2x2,
-                             BrDts: List[Matrix2x2]) -> Lattice:
+                             BrDts: List[Matrix2x2], calc_atoms=True) -> Lattice:
         """
         Creates Lattice object describing superlattice (supercell)
         of the heterostructure
@@ -469,12 +479,13 @@ class Heterostructure:
         res.set_vectors(*full_XDt.T.tolist())
 
         # atoms
-        for i, lay, MDt in zip(range(len(lattices)), lattices, MDts):
-            # We stack the layers one above the other so atoms must be moved up
-            # by the sum of z-sizes of all elementary cells of layers below
-            z_offset = self.__get_z_offset(i)
-            z_offset /= full_XDt[2, 2]  # angstrom -> Direct coordinates
-            Heterostructure.__superlattice_add_atoms(res, lay, MDt, z_offset)
+        if calc_atoms:
+            for i, lay, MDt in zip(range(len(lattices)), lattices, MDts):
+                # We stack the layers one above the other so atoms must be moved up
+                # by the sum of z-sizes of all elementary cells of layers below
+                z_offset = self.__get_z_offset(i)
+                z_offset /= full_XDt[2, 2]  # angstrom -> Direct coordinates
+                Heterostructure.__superlattice_add_atoms(res, lay, MDt, z_offset)
 
         return res
 
@@ -573,7 +584,7 @@ class Heterostructure:
     def opt(self,
             max_el: int = 6,
             thetas: Optional[List[Optional[List[float]]]] = None,
-            algorithm: str = "moire",
+            algorithm: str = "fast",
             log: bool = False
             ) -> Result:
         """
@@ -599,8 +610,8 @@ class Heterostructure:
             All angles are in radians.
 
         algorithm : str, optional
-            Default: "moire"
-            Accepted values: "moire", "brute"
+            Default: "fast"
+            Accepted values: "fast", "direct"
 
         log : bool, optional
             Default: False
@@ -644,9 +655,9 @@ class Heterostructure:
         config.log = log
 
         XA, XBs = self.__get_lattice_matrices()
-        if algorithm == "moire":
+        if algorithm == "fast":
             thetas, ADt, additional = MoireFinder(XA, XBs, thetas_in, config).solve()
-        else:  # "brute"
+        else:  # "direct"
             thetas, ADt, additional = StrainOptimisator(XA, XBs, thetas_in, config).solve()
 
         res = self.calc(ADt, thetas)

supercell_core/result.py
@@ -52,7 +52,8 @@ class Result:
                  strain_tensors: List[Matrix2x2],
                  strain_tensors_wiki: List[Matrix2x2],
                  ADt: Matrix2x2,
-                 ABtrs: List[Matrix2x2]):
+                 ABtrs: List[Matrix2x2],
+                 atom_count = None):
         """
 
         Parameters
@@ -73,6 +74,7 @@ class Result:
         self.__strain_tensors_wiki = strain_tensors_wiki
         self.__ADt = ADt
         self.__ABtrs = ABtrs
+        self.__atom_count = len(self.superlattice().atoms()) if atom_count is None else atom_count
 
     def strain_tensors(self, wiki_definition=False) -> \
             List[Matrix2x2]:
@@ -148,7 +150,7 @@ class Result:
         -------
         int
         """
-        return len(self.superlattice().atoms())
+        return self.__atom_count
 
     def superlattice(self) -> Lattice:
         """

supercell_core/tests/test_heterostructure.py
@@ -75,7 +75,7 @@ class TestHeterostructure(ut.TestCase):
         with self.assertRaises(IndexError):
             h.get_layer(10)
 
-    def test_opt(self, algorithm="moire"):
+    def test_opt(self, algorithm="fast"):
         # graphene-NiPS3 low-strain angle 21.9, theta range 16-30-0.1
         graphene = sc.read_POSCAR(
             path.join(path.dirname(__file__), "../resources/vasp/graphene/POSCAR"),
@@ -101,10 +101,10 @@ class TestHeterostructure(ut.TestCase):
         self.assertAlmostEqual(res.max_strain(), 0.000608879275296, places=5)
         self.assertEqual(res.atom_count(), 552)
 
-    def test_opt_brute(self):
-        return self.test_opt(algorithm="brute")
+    def test_opt_direct(self):
+        return self.test_opt(algorithm="direct")
 
-    def test_opt_graphene_moire(self):
+    def test_opt_graphene_fast(self):
         graphene = sc.read_POSCAR(
             path.join(path.dirname(__file__), "../resources/vasp/graphene/POSCAR"),
             atomic_species=["C"])

commit b0c2d6dc35db70a2045c719738b75072730848fc

Fix bug (bad function call in heterostructure_opt) introduced in 9b5fe2c; bump version to 0.1.6

setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setuptools.setup(
     name='supercell_core',
-    version='0.1.5',
+    version='0.1.6',
     packages=setuptools.find_packages(),
     url='https://github.com/tnecio/supercell-core',
     license='GPLv3',

supercell_core/heterostructure_opt.py
@@ -121,7 +121,6 @@ class OptSolver:
                         thetas: Tuple[Angle, ...],
                         ADt: np.ndarray,
                         sts: List[Matrix2x2],
-                        XBrs: List[Matrix2x2]
                         ) -> None:
         """
         Checks if newly calculated result is better than the previous one
@@ -145,14 +144,6 @@ class OptSolver:
         new_res = (list(thetas), min_qty, XDt, sts)
         new_size = np.abs(np.linalg.det(XDt))
         if self.config.log:
-
-            for n, XBr in enumerate(XBrs):
-                BrDt = inv(XBr) @ self.XA @ ADt
-                BtrDt = np.round(BrDt)
-                for i in range(2):
-                    for j in range(2):
-                        self.log["N{}_{}{}".format(n+1, i+1, j+1)].append(BtrDt[i, j])
-
             for i, theta in enumerate(thetas):
                 self.log["theta_{}".format(i)].append(theta)
             self.log["max_strain"].append(min_qty)
@@ -369,7 +360,7 @@ class MoireFinder(OptSolver):
             sts = [self._calculate_strain_tensor(ADt, XBr) for XBr in XBrs]
             # TODO: move to update_opt_res
 
-            self._update_opt_res(theta_comb, ADt, sts, XBrs)
+            self._update_opt_res(theta_comb, ADt, sts)
 
         return self.get_result()
 